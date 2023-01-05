437f77d7a7219054280d513385b49abffe8cc882
kju0048 <108639261+kju0048@users.noreply.github.com>
2023-01-05 18:56:16 +0900

    Update 쉬운 계단 수.py
    [Silver I] Title: 쉬운 계단 수, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -30,5 +30,6 @@ for i in range(1, n):
     for j in range(1, 9):
         nL[i][j] = nL[i-1][j-1] + nL[i-1][j+1]
 
+        
 print(sum(nL[n-1])%1000000000)
 >>>>>>> 96df4213db17be9234b309045ea54ef0749ac768:백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py

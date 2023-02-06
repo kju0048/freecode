597162a0f93f5c402c3a720f8cfb94b83e5cb40a
kju0048 <108639261+kju0048@users.noreply.github.com>
2023-02-06 18:30:41 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 700 ms, Memory: 24872 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i=0, j=1; i<2*n; i++) {
+            for(int k=0; k<2*n; k++) {
+                System.out.print(k>=j && k<2*n-j?" ":"*");
+            }
+            if(i<n-1)
+                j++;
+            else
+                j--;
+            
+            System.out.println();
+        }     
+        sc.close();
+    }
+}

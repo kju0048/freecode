90354ab882596efefe6498e585059f8b5f0e48d0
kju0048 <108639261+kju0048@users.noreply.github.com>
2023-06-15 14:53:38 +0900

    Add files via upload

--- test1.py
+++ test1.py
@@ -1,12 +1,10 @@
 import socket
 import struct
-from PIL import Image
 import io
 from google.cloud import vision
-from google.cloud import vision
-from PIL import Image
-
+from PIL import Image, ImageFile
 import os
+import numpy as np
 
 
 def send_text_array(sock, text_array):
@@ -59,61 +57,66 @@ def recognize_text(image_path):
     else:
         return None
 
+def main():
+	ImageFile.LOAD_TRUNCATED_IMAGES = True
+	host = '' 
+	port = 7777 
 
-host = '' 
-port = 7777 
+	while(1):
+	    print("meaning")
+	    s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	    s.bind((host, port))
 
-while(1):
-    print("meaning")
-    s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((host, port))
+	    s.listen(True)
 
-    s.listen(True)
+	    conn, addr = s.accept()
+	    print('Connected by', addr)
+	    length = recvall(conn, 4)
+	    length = struct.unpack('!i', length)[0]
 
-    conn, addr = s.accept()
-    print('Connected by', addr)
-    length = recvall(conn, 4)
-    length = struct.unpack('!i', length)[0]
-
-    stringData = recvall(conn, int(length))
-    data = io.BytesIO(stringData)
-    im = Image.open(data)
-
-
-    im.save('/root/Desktop/sevc/received_image.png','PNG')
+	    stringData = recvall(conn, int(length))
+	    data = io.BytesIO(stringData)
+	    im = Image.open(data)
 
 
-
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/root/Desktop/sevc/proven-country-388105-2b1fa1639dd9.json'
+	    im.save('/root/Desktop/sevc/received_image.png','PNG')
 
 
 
-    image_path = r"/root/Desktop/sevc/received_image.png"
+	    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/root/Desktop/sevc/proven-country-388105-2b1fa1639dd9.json'
 
 
 
-    # 이미지에서 텍스트 인식
-    recognized_text = recognize_text(image_path)
-
-
-    if recognized_text:
-        # 추출된 텍스트에서 숫자와 연산자 추출
-        expression_list = []
-        for char in recognized_text:
-            if char.isdigit() or char in ['+', '-', 'x', '/']:
-                expression_list.append(char)
-
-        # 추출한 문자열 리스트를 결합하여 수식 문자열 생성
-        expression_str = ''.join(expression_list)
-        send_text_array(conn, expression_str)
-        print(expression_str)
-    else:
-        print("텍스트를 인식할 수 없습니다.")
+	    image_path = r"/root/Desktop/sevc/received_image.png"
 
 
 
+	    # 이미지에서 텍스트 인식
+	    recognized_text = recognize_text(image_path)
 
 
-    conn.close()
+	    if recognized_text:
+		# 추출된 텍스트에서 숫자와 연산자 추출
+		expression_list = []
+		for char in recognized_text:
+		    if char.isdigit() or char in ['+', '-', 'x', '/']:
+		        expression_list.append(char)
+
+		# 추출한 문자열 리스트를 결합하여 수식 문자열 생성
+		expression_str = ''.join(expression_list)
+		send_text_array(conn, expression_str)
+		print(expression_str)
+	    else:
+		print("텍스트를 인식할 수 없습니다.")
+
+
+
+
+
+	    conn.close()
+
+if __name__ == "__main__":
+	main()
+	os.execl(sys.executable, sys.executable, *sys.argv)
     
